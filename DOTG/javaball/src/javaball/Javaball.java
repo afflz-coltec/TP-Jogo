f8b089b2bd2f721efa8272315042f50c52200dc8
afflz <augustovirtual317@gmail.com>
2016-10-22 23:19:56 -0200

    only god knows

--- DOTG/javaball/src/javaball/Javaball.java
+++ DOTG/javaball/src/javaball/Javaball.java
@@ -9,10 +9,11 @@ import java.util.logging.Logger;
 
 public class Javaball extends Ponto {
 
-    private static double energy_trade = 0.01;
-    private static double limite_x = 20;
-    private static double limite_y = 20;
+    private static double energy_trade = 0.6;
+    private static double limite_x = 10;
+    private static double limite_y = 10;
 
+    boolean aftershook;
     private double massa;
     String name;
     private double Vx;
@@ -22,6 +23,7 @@ public class Javaball extends Ponto {
     play p;
     private double nextVx;
     private double nextVy;
+    private boolean colision;
 
     public Javaball(double x, double y, int cor, double massa, double vx, double vy, String nome) throws Exception {
         super(x, y, cor, massa / 1000);
@@ -30,6 +32,8 @@ public class Javaball extends Ponto {
         setVy(vy);
         nextVx = vx;
         nextVy = vy;
+        this.aftershook = false;
+        this.colision = false;
         this.name = nome;
         p = new play();
         this.setColor(new Color((float) Math.random(), (float) Math.random(), (float) Math.random()));
@@ -78,10 +82,17 @@ public class Javaball extends Ponto {
 
         for (Javaball ball : balls) {
             try {
-                this.verificarColisao_norm(this, ball, c);
+                if (this != ball) {
+
+                    this.verificarColisao_norm(this, ball, c);
+                }
             } catch (Exception ex) {
             }
         }
+        if (!this.colision && this.aftershook) {
+            this.aftershook = false;
+        }
+        colision = false;
 
         setX(this.getX() + this.getVx());
         setY(this.getY() + this.getVy());
@@ -99,14 +110,20 @@ public class Javaball extends Ponto {
     }
 
     public void verificarLimite(boolean c) {
-        double aux1 = this.getX() + this.getVx() + getRaio();
-        double aux2 = this.getY() + this.getVy() + getRaio();
-        if ((aux1 > Javaball.limite_x) || (aux1 < -Javaball.limite_x)) {
+        double aux1 = this.getX() + this.getRaio();
+        double aux2 = this.getY() + this.getRaio();
+
+        if (aux1 < -Javaball.limite_x + this.getRaio() * 2 && this.nextVx < 0) {
+            set(true, false, c, 0);
+        } else if (aux1 > Javaball.limite_x && this.nextVx > 0) {
             set(true, false, c, 0);
         }
-        if ((aux2 > Javaball.limite_y) || (aux2 < -Javaball.limite_y)) {
+        if (aux2 > Javaball.limite_y && this.nextVy > 0) {
+            set(false, true, c, 0);
+        } else if (aux2 < -Javaball.limite_y + this.getRaio() * 2 && this.nextVy < 0) {
             set(false, true, c, 0);
         }
+
     }
 
     public double raio() {
@@ -141,8 +158,26 @@ public class Javaball extends Ponto {
         double avr_x = (p1.getVx() + p2.getVx()) * Javaball.energy_trade;
         double avr_y = (p1.getVy() + p2.getVy()) * Javaball.energy_trade;
 
-        p1.nextVx = (-(p1.getVx() + avr_x));
-        p1.nextVy = (-(p1.getVy() + avr_y));
+        if ((p1.getVx() > 0) == (p2.getVx() > 0)) {
+            p1.nextVx = (p1.nextVx + avr_x) / 2;
+            p2.nextVx = (p1.nextVx + avr_x) / 2;
+        } else if ((p1.getVx() > 0) != (p2.getVx() > 0)) {
+            p1.nextVx = (p1.nextVx - avr_x) / 2;
+            p2.nextVx = (p1.nextVx - avr_x) / 2;
+        }
+        if ((p1.getVy() > 0) == (p2.getVy() > 0)) {
+            p1.nextVy = (p1.nextVy + avr_y) / 2;
+            p2.nextVy = (p1.nextVy + avr_y) / 2;
+        } else if ((p1.getVy() > 0) != (p2.getVy() > 0)) {
+            p1.nextVy = (p1.nextVy - avr_y) / 2;
+            p2.nextVy = (p1.nextVy - avr_y) / 2;
+        }
+
+        p1.colision = true;
+        p2.colision = true;
+        p1.aftershook = true;
+        p2.aftershook = true;
+
     }
 
     private void reset() {
@@ -151,18 +186,19 @@ public class Javaball extends Ponto {
     }
 
     public void verificarColisao_norm(Javaball p1, Javaball p2, boolean c) throws IOException, Exception {
-        double dx = Math.pow((p1.getX()+p1.getVx() - p2.getX()+p2.getVx()), 2);
-        double dy = Math.pow((p1.getY() + p1.getVy() - p2.getY()+p2.getVy()), 2);
-
+        double dx = Math.pow(((p1.getX()) - (p2.getX())), 2);
+        double dy = Math.pow(((p1.getY()) - (p2.getY())), 2);
+        double rx = Math.pow(((p1.getX() + p1.raio()) - (p2.getX() + p2.raio())), 2);
+        double ry = Math.pow(((p1.getY() + p1.raio()) - (p2.getY() + p2.raio())), 2);
         double d = Math.sqrt(dx + dy);
-        double r = p1.raio() + p2.raio();
+        double r = Math.sqrt(rx + ry);
         // Ocorreu uma colisao !!
-        if (d > r) {
-            System.out.println(p1.getVx());
-            System.out.println(p2.getVx());
+        if (d < r && !(p1.aftershook || p1.colision)) {
+            System.out.println(p1.name);
+            System.out.println(p2.name);
             System.out.println("colidiu");
             Javaball.m_change(p1, p2);
-        }
+        } 
         //p1.p.soltaOSom();
     }
 
@@ -171,13 +207,21 @@ public class Javaball extends Ponto {
         StdDraw.setXscale(-Javaball.limite_x, Javaball.limite_x);
         StdDraw.setYscale(-Javaball.limite_y, Javaball.limite_y);
 
-        boolean c = true;
+        boolean c = false;
 
         ArrayList<Javaball> balls = new ArrayList<Javaball>();
         Javaball p1;
-        p1 = new Javaball(2.0, 0, 0, 2.0, 0.01, 0.01, "bolinha1");
+        p1 = new Javaball(2.0, 0, 0, 1.0, 0.6, 0.2, "bolinha1");
         balls.add(p1);
-        p1 = new Javaball(-2.0, 0, 0, 2.0, 0.01, 0.01, "bolinha2");
+        p1 = new Javaball(1.0, 0, 0, 1.0, -0.4, -0.4, "bolinha2");
+        balls.add(p1);
+        p1 = new Javaball(0.0, 0, 0, 1.0, -0.2, 0.6, "bolinha3");
+        balls.add(p1);
+        p1 = new Javaball(-2.0, 0, 0, 1.0, 0.4, -0.3, "bolinha4");
+        balls.add(p1);
+        p1 = new Javaball(-2.0, 0, 0, 1.0, 0.8, -0.2, "bolinha5");
+        balls.add(p1);
+         p1 = new Javaball(2.0, 0, 0, 1.0, 0.4, -0.3, "bolinha5");
         balls.add(p1);
 
         Javaball mouse = new Javaball(4.0, -9.0, 0, 10.0, 0.7, 0.4, "bololo haha");
